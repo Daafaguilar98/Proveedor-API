8b344c56e0cfffff2e94ef1222c60d55cc463979
Diego Aguilar <daafaguilar98@gmail.com>
2018-05-18 23:17:27 -0500

    New release

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const products = require('./controllers/products')
+const contapyme = require('./controllers/contapyme')
 const PORT = process.env.PORT || 5000
 const app = express()
 
 products(app)
+contapyme(app)
 
 app.get('/', (req, res) => {
   res.send('Proveedor Api')
