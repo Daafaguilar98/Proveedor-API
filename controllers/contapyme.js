97c9d3735d0a9bbf31d44a377c804254be1c0814
Diego Aguilar <daafaguilar98@gmail.com>
2018-05-22 20:53:34 -0500

    Resolve contapyme logic

--- controllers/contapyme.js
+++ controllers/contapyme.js
@@ -6,13 +6,13 @@ module.exports = function (app) {
   app.get('/contapyme/sync', (req, res) => {
     const urlHttp = 'http://186.115.207.187:9000/datasnap/rest/TBasicoGeneral/GetAuth/'
     const urlData = JSON.stringify({"email":"ventas@elproveedor.com.co","password":"db7068a18fbc5a91dac038c5816d11ce"})
-    const urlToken = '//2000/'
+    const urlToken = '//2000'
     const url = `${urlHttp}${urlData}${urlToken}`
     axios.get(url)
-      .then((response) => {
-        if (response.result[0].encabezado.resultado === "true") {
-          token.hash = response.result[0].respuesta.datos.keyagente
-          res.send({ sync: true })
+      .then(response => {
+        if (response.data.result[0].encabezado.resultado) {
+          token.hash = response.data.result[0].respuesta.datos.keyagente
+          res.send({ sync: true, data: response.data.result[0] })
         } else {
           res.send({ sync: false })
         }
