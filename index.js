712b25311e7750f19e157c5fd7bfbf51f7b2dc77
Diego Aguilar <daafaguilar98@gmail.com>
2018-05-24 22:29:32 -0500

    new controller

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const products = require('./controllers/products')
+const customers = require('./controllers/customers')
+const orders = require('./controllers/orders')
 const contapyme = require('./controllers/contapyme')
 const PORT = process.env.PORT || 5000
 const app = express()
 
 products(app)
+customers(app)
+orders(app)
 contapyme(app)
 
 app.get('/', (req, res) => {
